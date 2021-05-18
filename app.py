f21d0bf4b39eef094a6a0bef57b54bc3308af5af
aswinsuresh790 <47837236+aswinsuresh790@users.noreply.github.com>
2021-05-18 15:53:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 import keras
 from PIL import Image, ImageOps
 import numpy as np
@@ -11,7 +10,7 @@ st.text("Upload a Image for image classification")
 
 from img_classification import teachable_machine_classification
 
-uploaded_file = st.file_uploader("Choose an Mmode image ...", type="jpg")
+uploaded_file = st.file_uploader("Choose an Mmode image ...", type=['png','jpeg','jpg','dcm'])
 if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image.', use_column_width=True)
@@ -20,7 +19,11 @@ if uploaded_file is not None:
 
         hf = h5py.File('F:/streamlit-trial/VGG16Mmodegood.h5', 'r')
         label = teachable_machine_classification(image, hf)
-        if label == 0:
-            st.write("The MRI scan has a brain tumor")
+        if label == 1:
+            st.write("The scan is of cardeogenic pulmonary edema")
+        if label == 2:
+            st.write("The scan is of normal lung")
         else:
-            st.write("The MRI scan is healthy")
+            st.write("The scan is of pleural effusion")
+        #st.write("The scan is of ", label)
+
